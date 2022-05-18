d44b06942ca555321c32e20d5394f9a5b5b8afc6
Dimitar Rachev <DesolateBG@gmail.com>
2022-05-18 11:10:17 +0300

    Solved problems

--- src/JavaAdvanced/p14GenericsExercises/p11Threeuple/Main.java
+++ src/JavaAdvanced/p14GenericsExercises/p11Threeuple/Main.java
@@ -14,7 +14,7 @@ public class Main {
         String name = address[0] + " " + address[1];
         System.out.println(new Threeuple<>(name, address[2], address[3]));
 
-        System.out.println(new Threeuple<>(beer[0], Integer.parseInt(beer[1]), (beer[2])));
+        System.out.println(new Threeuple<>(beer[0], Integer.parseInt(beer[1]), beer[2].equals("drunk") ? "true" : "false"));
 
         System.out.println(new Threeuple<>(nums[0], Double.parseDouble(nums[1]), nums[2]));
 
